f8eb13a7a795f05fcf0957f6a8ff464778b5fb11
ZijiaLiuBSU <zijialiu677@gmail.com>
2026-03-10 20:17:53 -0400

    original lab6

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+// TODO: Add information about this assignment here, including your name.
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
